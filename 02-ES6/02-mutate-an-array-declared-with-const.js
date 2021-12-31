4234ddbb48d8d6901b88f773ae7bec8fc862c3a9
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-30 23:42:35 -0300

    Mutate an Array Declared with const.

---
+++ 02-ES6/02-mutate-an-array-declared-with-const.js
@@ -0,0 +1,10 @@
+// Mutate an Array Declaredwith const.
+const s = [5, 7, 2];
+
+function editInPlace() {
+    "use strict";
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+console.log(editInPlace());
