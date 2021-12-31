74be3096062a6d5e198d352bbc7f39c5abe5caea
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-30 23:08:48 -0300

    Compare Scopes of the var and let Keywords.

---
+++ 02-ES6/01-compare-scopes-of-the-var-and0let-keywords.js
@@ -0,0 +1,11 @@
+// Compare Scopes of the var and letKeywords.
+function checkScope() {
+    "use strict";
+    let i = "function scope";
+    if (true) {
+        let i = "block scope";
+        console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+}
