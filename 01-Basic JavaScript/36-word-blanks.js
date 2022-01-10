19fa1b5432c702ddbf961703fe2b9b1daf211ff2
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2022-01-10 20:17:17 -0300

    Word Blanks.

---
+++ 01-Basic JavaScript/36-word-blanks.js
@@ -0,0 +1,10 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "The " + myAdjective + " " +
+    myNoun + " " + myVerb + " " +
+    myAdverb + ".";
+
+console.log(wordBlanks);
